f3606be5162b75a684c76374abdc85f4fd404e09
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-01-03 20:10:44 +0900

    배경음악/효과음

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,6 +18,19 @@ public class GameManager : MonoBehaviour
     {
         textManager = GetComponent<TextManager>();
         dataManager = GetComponent<DataManager>();
+        DontDestroyOnLoad(gameObject);
+        #region 사운드
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
     }
 
     public void Start()
@@ -37,4 +51,39 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
+
+    public AudioSource bgsound;
+    public AudioClip[] bglist;
+
+    public static GameManager instance;
+
+    #region 사운드
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for (int i = 0; i < bglist.Length; i++)
+        {
+            if (arg0.name == bglist[i].name)
+                BgsoundPlay(bglist[i]);
+        }
+    }
+
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound");
+        AudioSource audioSource = go.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = 0.3f;
+        audioSource.Play();
+        Destroy(go, clip.length);
+    }
+
+    public void BgsoundPlay(AudioClip clip)
+    {
+        bgsound.clip = clip;
+        bgsound.loop = true;
+        bgsound.volume = 0.1f;
+        bgsound.Play();
+    }
+    #endregion
 }
