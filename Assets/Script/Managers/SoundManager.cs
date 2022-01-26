22197157b1649e3d79c8c64a077a1191afede542
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-01-26 13:01:50 +0900

    사운드 매니저 수정

--- Assets/Script/Managers/SoundManager.cs
+++ Assets/Script/Managers/SoundManager.cs
@@ -21,15 +21,17 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] Music[] BGM;
     [SerializeField] EffectMusic[] EffectMusics;
+    [SerializeField] AudioSource audioBGMPlayer;
     [SerializeField] AudioSource audioPlayer;
+
 
 
     public void PlayingMusic(int num, float volume)
     {
-        audioPlayer.clip = BGM[num].audio;
-        audioPlayer.loop = true;
-        audioPlayer.volume = volume;
-        audioPlayer.Play();
+        audioBGMPlayer.clip = BGM[num].audio;
+        audioBGMPlayer.loop = true;
+        audioBGMPlayer.volume = volume;
+        audioBGMPlayer.Play();
     }
 
     public void PlayingEffectMusic(int num, float volume)
@@ -43,10 +45,13 @@ public class SoundManager : MonoBehaviour
     public void PauseMusic()
     {
         audioPlayer.Pause();
+        audioBGMPlayer.Pause();
     }
 
     public void DeleteMusic()
     {
         audioPlayer.clip = null;
+        audioBGMPlayer.clip = null;
+
     }
 }
