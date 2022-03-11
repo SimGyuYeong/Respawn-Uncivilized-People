60c28804efda30b05defec984978e096919dce6b
meng-joo <81345410+meng-joo@users.noreply.github.com>
2022-03-11 12:11:51 +0900

    접으셈

---
+++ Assets/Script/UI/MiniMap/CafeStore.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CafeStore : MonoBehaviour
+{
+    Tweener store;
+    public GameObject StorePanel = null;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void Help()
+    {
+        
+    }
+}
