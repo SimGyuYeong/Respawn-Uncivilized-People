2905ee0d0b6e9b55601d79b1c589d0eee3a12956
meng-joo <81345410+meng-joo@users.noreply.github.com>
2022-03-14 13:07:14 +0900

    접으셈

--- Assets/Script/UI/MiniMap/CafeStore.cs
+++ Assets/Script/UI/MiniMap/CafeStore.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class CafeStore : MonoBehaviour
 {
+
+    
     Tweener store;
-    public GameObject StorePanel = null;
+    public GameObject storePanel = null;
+    public GameObject itemImage;
+    public Image storeColor;
+    public Slider gay;
+
     private void Start()
     {
-        
+        gay.value = 3;
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            storePanel.transform.DOMoveX(-20, 0.4f);
+            storeColor.DOFade(0, 0.3f);
+        }
     }
 
-    public void Help()
+    public void storebutton()
     {
-        
+        storePanel.transform.DOMove(Vector3.zero, 0.4f);
+        storeColor.DOFade(1, 0.3f);
+        gay.value--;
     }
 }
