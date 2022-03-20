9a0b3d9e32bbfe9c6473c7ff69a15780db6e0c3d
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2022-03-20 18:13:05 +0900

    전투 - A* 알고리즘, 플레이어 이동 프로토타입

--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -1,66 +1,104 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Tile : MonoBehaviour
-//{
-//    public GameObject _highlight;
-//    bool _active = true;
-//    public int tileNum;
+[System.Serializable]
+public class TileInform
+{
+    public TileInform(int _num, int _x, int _y, bool _wall)
+    {
+        tileNum = _num;
+        x = _x;
+        y = _y;
+        isWall = _wall;
+    }
 
-//    public void notActive()
-//    {
-//        _active = false;
-//    }
+    public int tileNum, x, y;
+    public bool isWall;
+}
 
-//    public void enter(bool ck)
-//    {
-//        SpriteRenderer color = gameObject.GetComponent<SpriteRenderer>();
-//        if (_active)
-//        {
-//            if (ck)
-//            {
-//                if (gameObject.transform.childCount >= 2)
-//                    color.color = Color.red;
-//                else
-//                   color.color = Color.yellow;
-//            }
-                
-//            else
-//                color.color = Color.white;
-//        }
-//    }
+public class Tile : MonoBehaviour
+{
+    public GameObject _highlight;
+    public TileInform tile;
 
-//    void OnMouseEnter()
-//    {
-//        if (_active)
-//            _highlight.SetActive(true);
-//    }
+    public void enter(bool ck)
+    {
+        if(FightManager.Instance.move == false)
+        {
+            SpriteRenderer color = gameObject.GetComponent<SpriteRenderer>();
+            if (!tile.isWall)
+            {
+                if (ck)
+                {
+                    if (gameObject.transform.childCount >= 2)
+                        color.color = Color.red;
+                    else
+                        color.color = Color.yellow;
+                }
 
-//    void OnMouseExit()
-//    {
-//        if (_active)
-//            _highlight.SetActive(false);
-//    }
-
-//    void OnMouseDown()
-//    {
-//        if(gameObject.transform.childCount >= 2)
-//        {
-//            if (gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().color == FightManager.Instance.Player.GetComponent<SpriteRenderer>().color)
-//            {
-//                FightManager.Instance.csTileNum = tileNum;
-//                FightManager.Instance.OnClickPlayer();
-//            }
-//        }
+                else
+                    color.color = Color.white;
+            }
+        }
         
-//        if (FightManager.Instance._playerClick)
-//        {
-//            if (_active)
-//            {
-//                if(FightManager.Instance.csTileNum != tileNum)
-//                    FightManager.Instance.PlayerMove(tileNum);
-//            }
-//        }
-//    }
-//}
+    }
+
+    void OnMouseEnter()
+    {
+        if (FightManager.Instance.move == false)
+        {
+            if (!tile.isWall)
+            {
+                _highlight.SetActive(true);
+                FightManager.Instance.targetPos.x = tile.x;
+                FightManager.Instance.targetPos.y = tile.y;
+                FightManager.Instance.PathFinding();
+            }
+        }
+            
+            
+    }
+
+    void OnMouseExit()
+    {
+        if (FightManager.Instance.move == false)
+        {
+            if (!tile.isWall)
+                _highlight.SetActive(false);
+        }
+            
+    }
+
+    private void OnMouseDown()
+    {
+        if (FightManager.Instance.move == false)
+        {
+            _highlight.SetActive(false);
+            FightManager.Instance.move = true;
+            StartCoroutine(FightManager.Instance.movePlayer());
+        }
+            
+    }
+
+    //void OnMouseDown()
+    //{
+    //    if (gameObject.transform.childCount >= 2)
+    //    {
+    //        if (gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().color == FightManager.Instance.Player.GetComponent<SpriteRenderer>().color)
+    //        {
+    //            FightManager.Instance.csTileNum = tileNum;
+    //            FightManager.Instance.OnClickPlayer();
+    //        }
+    //    }
+
+    //    if (FightManager.Instance._playerClick)
+    //    {
+    //        if (_active)
+    //        {
+    //            if (FightManager.Instance.csTileNum != tileNum)
+    //                FightManager.Instance.PlayerMove(tileNum);
+    //        }
+    //    }
+    //}
+}
