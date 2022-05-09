51d939bac80f9ab54edf1b93ef5238fc0a92f36f
meng-joo <81345410+meng-joo@users.noreply.github.com>
2022-05-09 23:20:33 +0900

    freetime_Mengjoo

---
+++ Assets/01.Script/FreeTime/FreeTimeText.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeTimeText : MonoBehaviour
+{
+    public string chat = "안녕 내 이름은 심규영. 내 폰트는 ☆겁나구려☆";
+
+    
+}
