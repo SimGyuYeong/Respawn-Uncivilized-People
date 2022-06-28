3ea2c079550a1cf902977a98604b52a758437dda
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-06-29 00:50:53 +0900

    사운드 매니저 만드는 중~

--- Assets/01.Script/Fight/Core/FightSoundManager.cs
+++ Assets/01.Script/Fight/Core/FightSoundManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class FightSoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] // BGM 출력 전용 오디오 소스
+    private AudioSource BGM;
+
+    [SerializeField] // Effect 출력 전용 오디오 소스
+    private AudioSource Effect1;
+
+    [SerializeField] // Effect 출력 전용 서브 오디오 소스
+    private AudioSource Effect2;
+
+    [SerializeField] // 배경음악, 효과음 등등 오디오 소스 배열
+    private AudioClip[] Sounds;
+
+    enum SoundsIndexNumber 
+    {
+        BGM,
+        GameOver,
+        PlayerMove,
+        EnemyMove,
+        ButtonClick,
+        PlayerClick,
+        TurnEndButton,
+        AttackEffect,
+        Effect2
+    };
+
+    private void Awake()
+    {
+
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BGMSet()
     {
-        
+        BGM.clip = Sounds[0];
     }
 }
