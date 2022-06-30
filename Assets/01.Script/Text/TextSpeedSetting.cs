8894bf0b77756fe0cfc3c416839b5fbca5e5005d
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2022-06-30 18:34:29 +0900

    백업본

--- Assets/01.Script/Text/TextSpeedSetting.cs
+++ Assets/01.Script/Text/TextSpeedSetting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextSpeedSetting
+public class TextSpeedSetting : MonoBehaviour
 {
     public void SetTextSpeed(float volume)
     {
